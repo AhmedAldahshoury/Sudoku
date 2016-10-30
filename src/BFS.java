d776f0d15f86b0ab6aff7cbe5fe1817aca5d04ad
AhmedAldahshoury <ahmed.aldahshoury@gmail.com>
2016-10-30 10:37:38 +0100

    fix BFS and create MRV

--- src/BFS.java
+++ src/BFS.java
@@ -3,18 +3,29 @@ import java.util.Queue;
 
 public class BFS {
 	
-	static Queue<short[][]> unVisited = new LinkedList<short[][]>();
-
-	public static void search(){
-		if(badie array is empty)
-		Finished,else
-		find the Zeros
-		short[badie][badie] state = unVisited.add(state);
-		
-		
-	}
+//	SudokuProcessor processor;
+//	short[][] sudoku;
+//	Queue<short[][]> unVisited = new LinkedList<short[][]>();
+//	
+//	public BFS(SudokuProcessor processor, short[][] sudoku) {
+//		this.processor = processor;
+//		this.sudoku = sudoku;
+//	}	
+//	
+//	public void search(GridSquare currentSquare){
+//		if(!this.processor.hasEmptySquares(currentSquare)){
+//			this.processor.isValid(this.sudoku);
+//		}
+//		else{
+//			
+//		}
+//		find the Zeros
+//		short[badie][badie] state = unVisited.add(state);
+//		
+//		
+//	}
 //
-//
+	//
 //	public static void BFSConstruction(short[][] state ){
 //		
 //		state = unVisited.poll();
---
+++ src/MRV.java
@@ -0,0 +1,27 @@
+
+public class MRV {
+
+	SudokuProcessor processor;
+	short[][] sudoku;
+	
+	public MRV(SudokuProcessor processor, short[][] sudoku) {
+		this.processor = processor;
+		this.sudoku = sudoku;
+	}
+	
+	public void search(){
+		
+//		while(processor.hasEmptySquares(sudoku)){
+//		processor.getPossibleMoves(sudoku, x, y);
+//		
+//		}
+//		
+//		loop on every empty square and get his possible moves 
+//		then add this empty square to the priority queue with his
+//		possible moves as the priority index
+//		as a result we will have a priority queue with the most constrained variables first
+//		
+		
+		
+	}
+}
